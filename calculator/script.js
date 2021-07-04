b8ee2534858bac9ce7648ff19bf6d7f3523bf566
Daniel Baker <dannyb1510@outlook.com>
2021-07-04 17:29:27 +0100

    updated javascript, calc now functional

--- calculator/script.js
+++ calculator/script.js
@@ -1,62 +1,115 @@
-let displayValue = '0';
+let displayValue = "0";
 let firstInput = null;
 let operator = null;
-let secondInput = null;
+let secondInput = false;
+// console.log(displayValue);
+// console.log(firstInput);
+// console.log(operator);
+// console.log(secondInput);
+
+function number(digit) {
+    if (displayValue === "0" || secondInput === true) {
+        displayValue = digit;
+        secondInput = false;
+    } else {
+        displayValue += digit;
+    }
+    console.log(displayValue);
+    console.log(firstInput);
+    console.log(operator);
+    console.log(secondInput);
+}
+
+
+function decimal(dot) {
+    if (!displayValue.includes(dot)) {
+        displayValue += dot;
+    }
+}
+
+
+function handleOperator(operatorPressed) {
+    const inputValue = parseFloat(displayValue);
+    if (firstInput === null && !isNaN(inputValue)) {
+        firstInput = inputValue;
+    } else if (operator) {
+        const result = calculation(firstInput, inputValue, operator);
+
+        displayValue = String(result);
+        firstInput = result;
+    }
+
+    secondInput = true;
+    operator = operatorPressed;
+
+    console.log(displayValue);
+    console.log(firstInput);
+    console.log(operator);
+    console.log(secondInput);
+}
+
+
+function calculation(firstInput, secondInput, operator) {
+    if (operator === "*") {
+        return firstInput * secondInput;
+    }
+    if (operator === "/") {
+        return firstInput / secondInput;
+    }
+    if (operator === "+") {
+        return firstInput + secondInput;
+    }
+    if (operator === "-") {
+        return firstInput - secondInput
+    }
+    return secondInput;
+}
+
 
 function updateScreen() {
     let display = document.querySelector(".calculator__screen");
     display.value = displayValue;
     }
     
-    updateScreen();
-
-
-function calculation(firstInput, operator, secondInput) {
-    if (operator === "*") {
-        displayValue = firstInput * secondInput;
-    }
-    if (operator === "/") {
-        displayValue = firstInput / secondInput;
-    }
-    if (operator === "+") {
-        displayValue = firstInput + secondInput;
-    }
-    if (operator === "-") {
-        displayValue = firstInput - secondInput
-    }
-    updateScreen();
-}
+updateScreen();
 
 
 const buttons = document.querySelector(".calculator__keys");
 buttons.addEventListener("click", (event) => {
 
+    if (!event.target.matches("button")) {
+        return;
+    }
+
     if (event.target.classList.contains("operator")) {
-        displayValue = event.target.value;
-        // firstInput = displayValue;
-        // console.log(firstInput);
+        handleOperator(event.target.value);
+        updateScreen();
+        return;
     }
 
     if (event.target.classList.contains("decimal")) {
-        displayValue = event.target.value;
+        decimal(event.target.value);
+        updateScreen();
+        return;
     }
 
     if (event.target.classList.contains("clear")) {
         displayValue = "0";
         firstInput = null;
-        secondInput = null;
+        secondInput = false;
         operator = null;
+
+        console.log(displayValue);
+        console.log(firstInput);
+        console.log(operator);
+        console.log(secondInput);
+        updateScreen();
+        return;
     }
 
-    else {
-        displayValue = event.target.value;
-    }
-
+    number(event.target.value);
     updateScreen();
-})
+});
 
-function input() {
-    if (displayValue === "0") {
-        
-    }
-}
+
+
